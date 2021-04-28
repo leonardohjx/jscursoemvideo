9a6db448325e26c6be648a3d3b75359d9832b4f5
leonardohjx <52010690+leonardohjx@users.noreply.github.com>
2021-04-28 16:18:08 -0300

    Commit novas aulas

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 70 ){
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}else{
+    console.log(`Dirija sempre usando cinto de segurança`)
+}
---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 15
+if(idade < 16) {
+    console.log(`Como sua idade é ${idade}, você não pode voltar.`)
+} else {
+    if(idade >= 18){
+    console.log(`Como sua idade é ${idade}, você tem o voto obrigatório. `)
+    } else {
+        console.log(`Como sua idade é ${idade}, você tem o voto opicional. `)
+    }
+}
